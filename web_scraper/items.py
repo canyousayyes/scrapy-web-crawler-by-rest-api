aa376626ea66a2774e64c54bfacb10b963af8078
Gene Ng <fishballium@gmail.com>
2019-03-04 11:56:28 +0800

    Define item schema

--- web_scraper/items.py
+++ web_scraper/items.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
+from scrapy.loader.processors import TakeFirst, Join, Compose
 
 
-class WebScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class Article(Item):
+    title = Field(output_processor=TakeFirst())
+    publish_date = Field(output_processor=TakeFirst())
+    content = Field(
+        output_processor=Compose(lambda v: filter(None, v), Join(''))
+    )
+    image_urls = Field()
+    links = Field()
     pass
