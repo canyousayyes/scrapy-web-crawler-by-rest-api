73ad125672eb99f654e06fc4c3375f2e3ad11452
Gene Ng <fishballium@gmail.com>
2019-03-04 11:56:43 +0800

    Parse item from JSON response

--- web_scraper/spiders/techcrunch.py
+++ web_scraper/spiders/techcrunch.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-import re
-import scrapy
 from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+from scrapy.http import HtmlResponse
 from scrapy.spiders import CrawlSpider, Rule
+from web_scraper.items import Article
+import json
+import re
 
 
 def process_value(value):
@@ -31,8 +34,21 @@ class TechcrunchSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        json_res = json.loads(response.body)
+        if not isinstance(json_res, list) or len(json_res) < 1:
+            return None
+
+        data = json_res[0]
+        content = HtmlResponse(
+            response.url,
+            body=bytes(data['content']['rendered'], 'utf-8')
+        )
+
+        loader = ItemLoader(item=Article(), response=content)
+        loader.add_value('title', data['title']['rendered'])
+        loader.add_value('publish_date', data['date_gmt'])
+
+        loader.add_css('content', '*::text')
+        loader.add_css('image_urls', 'img::attr(src)')
+        loader.add_css('links', 'a::attr(href)')
+        return loader.load_item()
